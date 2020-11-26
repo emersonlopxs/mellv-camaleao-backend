c1a4c9bd1bbb4f9d874342153d1d08e374df3cb6
emersonlopxs <emersonlopxs@gmail.com>
2020-11-26 20:53:31 -0300

    f

--- src/utils/createToken.js
+++ src/utils/createToken.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 module.exports = function (id) {
-        const token = jwt.sign({ id }, process.env.SECRET, {
+        const token = jwt.sign({ id }, 'addsasds', {
             expiresIn: '90d' // expires in 10min
         })
         return token
